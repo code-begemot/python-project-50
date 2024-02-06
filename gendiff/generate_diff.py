505b7c23315cc2f6e45ff67743b7882249f724fc
code-begemot <152194874+code-begemot@users.noreply.github.com>
2024-02-06 11:39:01 +0500

    Update generate_diff.py
    Change format to format_

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,12 +3,12 @@ from gendiff.file_parser import file_parser
 from gendiff.file_formatter import file_formatter
 
 
-def generate_diff(file_path_1, file_path_2, format='stylish'):
+def generate_diff(file_path_1, file_path_2, format_='stylish'):
     # parsing files
     file_data_1 = file_parser(file_path_1)
     file_data_2 = file_parser(file_path_2)
     # building
     diff = calculate_diff(file_data_1, file_data_2)
     # format output
-    result = file_formatter(diff, format)
+    result = file_formatter(diff, format_)
     return result
