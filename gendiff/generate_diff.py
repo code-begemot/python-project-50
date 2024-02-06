ea34bf97b42644a26e4028dbbb2b915ac4dd3e55
code-begemot <152194874+code-begemot@users.noreply.github.com>
2024-02-06 08:45:34 +0500

    Update generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,7 +3,7 @@ from gendiff.file_parser import file_parser
 from gendiff.file_formatter import file_formatter
 
 
-def generate_diff(file_path_1, file_path_2, format):
+def generate_diff(file_path_1, file_path_2, format='stylish'):
     # parsing files
     file_data_1 = file_parser(file_path_1)
     file_data_2 = file_parser(file_path_2)
